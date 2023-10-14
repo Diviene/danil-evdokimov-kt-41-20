e0a0769f39219028a29a62f54a00e2e607801b22
ariel19107995@gmail.com <ariel19107995@gmail.com>
2023-10-14 16:04:03 +0300

    - updated student.cs model

--- WeatherForecast/Models/Student.cs
+++ WeatherForecast/Models/Student.cs
@@ -19,24 +19,5 @@ namespace WeatherForecast
         public Group? Group { get; set; }
         public ICollection<Grade>? Grades { get; set; }
         public ICollection<Test>? Tests { get; set; }
-
-        public bool IsAdult(string birthDateString)
-        {
-            var birthDateRegex = new Regex(@"\d{4}-\d{2}-\d{2}");
-
-            if (!birthDateRegex.IsMatch(birthDateString))
-            {
-                throw new ArgumentException("birthDateString должен быть в формате yyyy-mm-dd");
-            }
-
-            if (!DateTime.TryParse(birthDateString, out var birthDate))
-            {
-                throw new ArgumentException("birthDateString должен отображать правильную дату");
-            }
-
-            var eighteenYearsAgo = DateTime.Today.AddYears(-18);
-
-            return birthDate <= eighteenYearsAgo;
-        }
     }
 }
